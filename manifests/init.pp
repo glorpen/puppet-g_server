011c9bc57e0d2f491b0e2065645e37c759d66974
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 13:49:55 +0200

    firewall part 4

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,11 @@
 class g_server (
-  
+  $external_iface = undef,
+  $internal_ifaces = []
 ) {
+  
+  if ! $external_iface {
+    fail("No external iface given");
+  }
   
   class { 'g_server::firewall': }
 
