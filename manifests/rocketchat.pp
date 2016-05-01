e21558204134a1f39e7bde35a4f1bd75acb4d545
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 07:47:14 +0200

    added rocketchat and nodes definitions

---
+++ manifests/rocketchat.pp
@@ -0,0 +1,12 @@
+class g_server::rocketchat (
+){
+  package_keywords { 'net-libs/http-parser':
+	  keywords => ['~amd64'],
+	  target   => 'puppet',
+	  version  => '>=2.6.1',
+	  ensure   => present,
+	}->
+	package { 'net-im/rocketchat-server':
+	  ensure   => '0.28.0'
+	}
+}
---
+++ manifests/rocketchat/instance.pp
@@ -0,0 +1,6 @@
+define g_server::rocketchat::instance(
+  $site_url = undef,
+  $internal_port = undef,
+  $mongo_url = undef,
+){
+}
