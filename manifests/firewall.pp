dcb78a98737fe6ee099bd2ec2e5430141fb9ec0f
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 09:00:56 +0200

    fixed firewall config

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -7,12 +7,11 @@ class g_server::firewall(
 	resources { 'firewallchain':
 	  purge => true,
 	}
-	
 	Firewall {
 	  before  => Class['g_server::firewall::post'],
 	  require => Class['g_server::firewall::pre'],
 	}
-	
+  class { ['g_server::firewall::pre', 'g_server::firewall::post']: }
 	class { 'firewall': }
 	
 	
