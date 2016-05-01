aca0b2da9e0b33a15c548662941c41fc081c0d88
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 16:16:05 +0200

    firewall

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -13,4 +13,12 @@ class g_server::firewall(
 	}
   class { ['g_server::firewall::pre', 'g_server::firewall::post']: }
 	class { '::firewall': }
+	
+	firewall { '200 allow all external output':
+	  outiface => $::g_server::external_iface,
+    chain  => 'OUTPUT',
+    proto  => 'all',
+    action => 'accept',
+  }
+
 }
