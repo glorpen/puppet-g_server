1cd65bd336a9f9cb2a961a325685f7ecc5d5d60b
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 18:19:56 +0200

    install imagemagick

--- manifests/rocketchat.pp
+++ manifests/rocketchat.pp
@@ -1,15 +1,17 @@
 class g_server::rocketchat (
 ){
+	ensure_packages(['media-gfx/imagemagick'])
+	
   package_keywords { 'net-libs/http-parser':
 	  keywords => ['~amd64'],
 	  target   => 'puppet',
 	  version  => '>=2.6.1',
 	  ensure   => present,
 	}->
+	Package['media-gfx/imagemagick']->
 	package { 'net-im/rocketchat-server':
 	  ensure   => '0.28.0'
 	}~>
 	G_server::Rocketchat::Instance<| |>
-	
-	ensure_packages(["media-gfx/imagemagick"])
+
 }
