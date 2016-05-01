f1eebe6e0a2a38c29999e31a8c6eab93e161b732
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 16:19:27 +0200

    firewall

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -4,9 +4,7 @@ class g_server::firewall(
 	resources { 'firewall':
 	  purge => true,
 	}
-	#resources { 'firewallchain':
-	#  purge => true,
-	#}
+	
 	Firewall {
 	  before  => Class['g_server::firewall::post'],
 	  require => Class['g_server::firewall::pre'],
@@ -16,6 +14,13 @@ class g_server::firewall(
 	
 	firewall { '200 allow all external output':
 	  outiface => $::g_server::external_iface,
+    chain  => 'OUTPUT',
+    proto  => 'all',
+    action => 'accept',
+  }
+  
+  firewall { '201 allow all loopback output':
+    outiface => 'lo',
     chain  => 'OUTPUT',
     proto  => 'all',
     action => 'accept',
