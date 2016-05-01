4bd3fc7801ed92120dd80e5a07aca0a8a746c1b1
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-01 14:17:18 +0200

    firewall part 5

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -2,10 +2,10 @@ class g_server::firewall(
 ){
 
 	resources { 'firewall':
-	  #purge => true,
+	  purge => true,
 	}
 	resources { 'firewallchain':
-	  #purge => true,
+	  purge => true,
 	}
 	Firewall {
 	  before  => Class['g_server::firewall::post'],
@@ -13,21 +13,4 @@ class g_server::firewall(
 	}
   class { ['g_server::firewall::pre', 'g_server::firewall::post']: }
 	class { 'firewall': }
-	
-	
-	#allow ssh
-	firewall { '006 Allow inbound SSH':
-	  dport     => 22,
-	  proto    => tcp,
-	  action   => accept
-	}
-	
-	#todo temporary fix - register f2b chains
-	firewallchain { 'f2b-postfix:filter:IPv4':
-	   ensure => present
-  }
-  firewallchain { 'f2b-sshd:filter:IPv4':
-    ensure => present
-  }
-
 }
