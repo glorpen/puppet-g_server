a9c04a5cb9df1db895918aed90f85e9e08ecbab5
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-04 10:36:30 +0200

    nginx

---
+++ manifests/certs/domain.pp
@@ -0,0 +1,24 @@
+define g_server::certs::domain(
+  $domain = $title,
+  $vhost  = "${domain}"
+){
+  
+  contain(g_server::certs)
+  
+  file{ "/var/www/letsencrypt/${domain}":
+    ensure => "directory",
+    mode => "a=rx,u+w"
+  }->
+  nginx::resource::location { "${vhost}_letsencrypt":
+    ensure          => present,
+    ssl             => false,
+    vhost           => "${vhost}",
+    location => "/.well-known",
+    www_root => "/var/www/letsencrypt/${domain}"
+  }->
+  letsencrypt::certonly { $domain:
+    domains => [$domain],
+    plugin  => 'webroot',
+    webroot_paths => ["/var/www/letsencrypt/${domain}"]
+  }
+}
---
+++ manifests/nginx.pp
@@ -0,0 +1,36 @@
+class g_server::nginx(
+  $letsencrypt = true,
+  $external = true
+){
+  
+  if $external {
+	  firewall { "020 Allow external nginx":
+	    dport   => [80, 443],
+	    proto    => tcp,
+	    action   => accept,
+	    iniface  => $::g_server::external_iface
+	  }
+  }
+  
+  $::g_server::internal_ifaces.each |$iface| {
+    firewall { "020.${iface} Allow internal nginx":
+      dport   => [80, 443],
+      proto    => 'all',
+      action   => accept,
+      iniface  => $iface
+    }
+  }
+  
+  if $letsencrypt {
+	  nginx::vhost { 'localhost':
+	    ensure => present,
+	    listen_options => 'default_server',
+	    ssl => false,
+	    www_root => '/var/www/localhost',
+	    try_files => ['$uri', '/var/www/letsencrypt/$host/$uri']
+	  }
+	  
+	  class { 'g_server::certs': }
+  }
+  
+}
