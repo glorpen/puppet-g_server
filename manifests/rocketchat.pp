847aa96f95d50f84682ca4013dd133e3b60e591b
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-05 10:49:28 +0200

    rocketchat & mongo version bump

--- manifests/rocketchat.pp
+++ manifests/rocketchat.pp
@@ -8,9 +8,33 @@ class g_server::rocketchat (
 	  version  => '>=2.6.1',
 	  ensure   => present,
 	}->
+	package_keywords { 'dev-db/mongodb':
+    keywords => ['~amd64'],
+    target   => 'puppet',
+    version  => '=3.2.5',
+    ensure   => present,
+  }->
+  package_keywords { 'dev-libs/boost':
+    keywords => ['~amd64'],
+    target   => 'puppet',
+    version  => '=1.60.0',
+    ensure   => present,
+  }->
+  package_keywords { 'app-admin/mongo-tools':
+    keywords => ['~amd64'],
+    target   => 'puppet',
+    version  => '=3.2.5',
+    ensure   => present,
+  }->
+  package_keywords { 'dev-util/boost-build':
+    keywords => ['~amd64'],
+    target   => 'puppet',
+    version  => '=1.60.0',
+    ensure   => present,
+  }->
 	Package['media-gfx/imagemagick']->
 	package { 'net-im/rocketchat-server':
-	  ensure   => '0.28.0'
+	  ensure   => '0.29.0'
 	}~>
 	G_server::Rocketchat::Instance<| |>
 
