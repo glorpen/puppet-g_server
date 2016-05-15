db1ad8bca014ffefab10f82b06cfe1a3b706800e
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-15 23:49:35 +0200

    git

--- manifests/git/repo.pp
+++ manifests/git/repo.pp
@@ -13,6 +13,19 @@ define g_server::git::repo(
 	  }
   }
   
+  $parts = $title.split('/')
+  $parts[0:-1].each | $index, $part | {
+    $p = join($parts[0,$index+1],'/')
+    ensure_resource('file', "${::g_server::git::repo_dir}/${p}", {
+	    owner => $::g_server::git::user,
+	    group => $::g_server::git::group,
+	    recurse => true,
+	    purge => true,
+	    force => true,
+	    mode => '0700',
+    })
+  }
+  
   Exec['gitolite.refresh']->
   file { "${::g_server::git::repo_dir}/${title}.git":
     ensure => $ensure? {
