dc62b4b1ead9a26c0f55d8701dda42338a1b9a16
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-19 09:32:35 +0200

    fix

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -26,7 +26,7 @@ class g_server::mysql(
     g_portage::package_use { 'g_mysql':
       atom => 'dev-db/mariadb',
       use => ['-pam', '-perl']
-    }~>Class['Mysql::Server::Install']
+    }~>Package['mysql-server']
   }
 
 }
