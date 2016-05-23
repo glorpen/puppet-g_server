e61c81383bb64f263e34aa210e0bc400b110cb7b
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-05-23 17:10:11 +0200

    certbot name change

--- manifests/certs.pp
+++ manifests/certs.pp
@@ -25,53 +25,46 @@ class g_server::certs(
   g_portage::package_keywords { 'dev-python/parsedatetime@certs':
     atom => "=dev-python/parsedatetime-2.1",
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
   
   g_portage::package_keywords { 'dev-python/configargparse@certs':
     atom => "=dev-python/configargparse-0.10.0",
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
   
   g_portage::package_keywords { 'app-crypt/acme@certs':
     atom => "=app-crypt/acme-0.6.0",
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
   
   g_portage::package_keywords { 'dev-python/pyrfc3339@certs':
     atom => "=dev-python/pyrfc3339-1.0",
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
   
   g_portage::package_keywords { 'dev-python/psutil@certs':
     atom => "=dev-python/psutil-4.1.0",
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
   
   g_portage::package_keywords { 'dev-python/cryptography@certs':
     atom => "=dev-python/cryptography-1.3.1",
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
   
   g_portage::package_keywords { 'app-crypt/certbot@certs':
     atom => 'app-crypt/certbot',
     ensure   => present,
-    tag => 'puppet-certs',
     keywords => ['~amd64']
   }
 
-  G_portage::Package_keywords<| tag == 'puppet-certs' |>
+  G_portage::Package_keywords<| |>
   ~>Package['app-crypt/certbot']
   
   file{ "/var/www/letsencrypt":
