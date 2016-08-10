b4db9283aad3e79f4e306aa4a9ff753836d5c49d
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-08-10 23:51:17 +0200

    add mysql datadir option

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,32 +1,60 @@
 class g_server::mysql(
-  $port = '3306',
-  $root_password = undef
+  $port = undef,
+  $root_password = undef,
+  $datadir = undef
 ){
 
+  include ::stdlib
+  
   validate_string($root_password)
+  include ::mysql::params
+  
+  $_port = pick($port, $::mysql::params::default_options['mysqld']['port'])
+  $_datadir = pick($datadir, $::mysql::params::default_options['mysqld']['datadir'])
+  
+  if $datadir {
+    file { $datadir:
+      ensure => directory,
+      owner => $::mysql::params::default_options['mysqld']['user'],
+      group => $::mysql::params::default_options['mysqld']['user'],
+      notify => Class['mysql::server::service'],
+      require => Package['mysql-server']
+    }
+    
+    if $::facts['os']['selinux']['enabled'] {
+      selinux::fcontext { 'g-mysql-data-dir':
+			  context => 'mysqld_db_t',
+        pathname => "${datadir}(/.*)?",
+        restorecond_path => $datadir,
+        require => File[$datadir],
+        restorecond_recurse => true
+			}
+    }
+  }
 
   firewall { '010 Allow mysql from loopback':
-    dport     => $port,
+    dport     => $_port,
     proto    => tcp,
     action   => accept,
     iniface => 'lo'
   }->
 	class { '::mysql::server':
-   root_password => $root_password,
+    root_password => $root_password,
 	  remove_default_accounts => true,
 	  override_options => {
 	   'client'=> {
-	     'port' => $port,
+	     'port' => $_port,
 	   },
 	   'mysqld' => {
-	     'port' => $port,
+	     'port' => $_port,
+	     'datadir' => $_datadir
 	   }
 	  },
 	  restart => true
 	}->
- class {'::mysql::client':
-   package_manage => false
- }
+  class {'::mysql::client':
+    package_manage => false
+  }
 	
 	if $::osfamily == 'Gentoo' {
     g_portage::package_use { 'g_mysql':
