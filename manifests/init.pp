3043eb68be5d4715e07bc7911164968ec682aa48
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2016-08-19 19:52:31 +0200

    ssh and keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,6 +2,9 @@ class g_server (
   $external_iface = undef,
   $internal_ifaces = [],
   $turnserver = false,
+  
+  $admin_account = undef,
+  $admin_ssh_keys = []
 ) {
   
   if ! $external_iface {
@@ -26,5 +29,29 @@ class g_server (
   
   include ::g_server::repos
   
-  #$manage_repo 
+  #$manage_repo
+  
+  #
+  # + sudo
+  #
+  if $admin_account and $admin_ssh_keys {
+  
+    include ::g_server::ssh
+    
+	  user { $admin_account:
+	    ensure => 'present',
+	    home => "/home/${admin_account}",
+	    groups => [ $::g_server::ssh::group, 'wheel' ],
+	    managehome => true,
+	    purge_ssh_keys => true
+	  }
+	  
+	  $admin_ssh_keys.each | $place, $key | {
+		  ssh_authorized_key { "${admin_account}@${place}":
+			  user => $admin_account,
+			  type => 'ssh-rsa',
+			  key  => $key,
+			}
+		}
+	}
 }
---
+++ manifests/ssh.pp
@@ -0,0 +1,37 @@
+class g_server::ssh (
+  $group = 'ssh-users',
+  $ports = [22],
+){
+  
+  group { $group:
+    ensure => 'present',
+    system => true
+  }->
+  class { 'ssh::server':
+    storeconfigs_enabled => false,
+    options => {
+      'UsePAM' => 'yes',
+      'GSSAPIAuthentication' => 'no',
+
+      'ChallengeResponseAuthentication' => 'no',
+      'PasswordAuthentication' => 'no',
+      'PermitRootLogin'        => 'no',
+      'Port'                   => $ports,
+      
+      'Subsystem' => 'sftp  /usr/libexec/openssh/sftp-server',
+      
+      "Match Group ${group}" => {
+        'PasswordAuthentication' => 'yes',
+        'AllowTcpForwarding' => 'yes',
+        'X11Forwarding' => 'yes',
+      },
+      
+      'AcceptEnv' => [
+        'LANG LC_CTYPE LC_NUMERIC LC_TIME LC_COLLATE LC_MONETARY LC_MESSAGES',
+        'LC_PAPER LC_NAME LC_ADDRESS LC_TELEPHONE LC_MEASUREMENT',
+        'LC_IDENTIFICATION LC_ALL LANGUAGE',
+        'XMODIFIERS',
+      ],
+    }
+  }
+}
