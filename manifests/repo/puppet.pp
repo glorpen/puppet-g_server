80a44ae182b65f574992d4d3fd7f8a25b0cdd4ba
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-07-04 17:12:33 +0200

    added puppet repo

---
+++ manifests/repo/puppet.pp
@@ -0,0 +1,24 @@
+class g_server::repo::puppet(
+  $version = 5,
+  $priority   = 99,
+){
+  $osver = $::operatingsystem ? {
+    'XenServer' => [ '5' ],
+    default     => split($::operatingsystemrelease, '[.]')
+  }
+  $release = $::operatingsystem ? {
+    /(?i:Centos|RedHat|Scientific|CloudLinux|XenServer)/ => $osver[0],
+    default                                              => '6',
+  }
+
+  yum::managed_yumrepo { 'puppetx':
+    descr          => "Puppet ${version} Repository el ${release} - \$basearch",
+    baseurl        => "http://yum.puppetlabs.com/puppet${version}/el/${release}/\$basearch",
+    enabled        => 1,
+    gpgcheck       => 1,
+    failovermethod => 'priority',
+    gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-puppetX',
+    gpgkey_source  => 'puppet:///modules/g_server/rpm-gpg/RPM-GPG-KEY-puppet${version}',
+    priority       => $priority,
+  }
+}
