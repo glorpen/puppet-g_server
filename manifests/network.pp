66f15a76f5ceafaf9c4617acbf5d708637f07459
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-07-19 12:13:09 +0200

    pass additional hosts

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,6 +1,6 @@
 class g_server::network(
   String $export_tld = 'internal',
-  Hash $additional_hosts = {},
+  Array $additional_hosts = [],
 ){
   class { 'network':
     ipv6enable => true,
@@ -8,12 +8,6 @@ class g_server::network(
   
   class { 'hosts':
     enable_fqdn_entry => false,
-  }
-  
-  $additional_hosts.each | $name, $conf | {
-    hosts::host { "${::fqdn}-${conf['ip']}-${name}":
-      name => $name,
-      * => $conf
-    }
+    hosts => $additional_hosts
   }
 }
