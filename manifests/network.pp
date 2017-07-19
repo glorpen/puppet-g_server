a37ad08756719d2ac97b2bdff689852dca6416a9
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-07-19 08:25:24 +0200

    added additional hosts parameter

--- manifests/network.pp
+++ manifests/network.pp
@@ -1,5 +1,6 @@
 class g_server::network(
   String $export_tld = 'internal',
+  Hash $additional_hosts = {}
 ){
   class { 'network':
     ipv6enable => true,
@@ -7,5 +8,12 @@ class g_server::network(
   
   class { 'hosts':
     purge_hosts => true
+  }
+  
+  $additional_hosts.each | $name, $conf | {
+    host { "${::fqdn}-${name}",
+      name => $name,
+      * => $conf
+    }
   }
 }
