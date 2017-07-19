653cf3ffb2878d65698b58923523070781f87283
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2017-07-19 08:12:55 +0200

    oops

--- functions/get_interfaces.pp
+++ functions/get_interfaces.pp
@@ -5,6 +5,7 @@ function g_server::get_interfaces(G_server::Side $side) >> Array {
   return $side ? {
     'internal' => $::g_server::internal_ifaces,
     'external' => $::g_server::external_ifaces,
+    'both' => concat($::g_server::internal_ifaces, $::g_server::external_ifaces),
     default => []
   }
 }
