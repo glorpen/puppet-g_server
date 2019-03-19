d7a4a30106019f1f8b17fcaaea489798400357c8
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-03-19 21:34:56 +0100

    added ensure parameter to volumes::thinpool

--- manifests/volumes/thinpool.pp
+++ manifests/volumes/thinpool.pp
@@ -1,11 +1,12 @@
 define g_server::volumes::thinpool(
+  Enum['present','absent'] $ensure = 'present',
   String $lv_name = $title,
   String $vg_name,
   String $size,
   Optional[String] $metadata_size = undef
 ){
   lvm::logical_volume { $lv_name:
-    ensure       => present,
+    ensure       => $ensure,
     volume_group => $vg_name,
     size         => $size,
     mountpath_require => false,
