978afb555328519f7db16ba327cbe6983f278496
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-05-21 12:06:54 +0200

    added support for tuntap interfaces

---
+++ manifests/network/tuntap.pp
@@ -0,0 +1,18 @@
+define g_server::network::tuntap(
+  $ipv4addr = undef,
+  $ipv4netmask = undef,
+  $ipv6addr = undef,
+  $ipv6gw = undef
+) {
+  $ipv6init = $ipv6addr?{undef=>'no', default=>'yes'}
+
+  ::network::interface { $name:
+      type           => 'Tap',
+      ipaddress      => $ipv4addr,
+      netmask        => $ipv4netmask,
+      ipv6init       => $ipv6init,
+      ipv6_autoconf  => false,
+      ipv6addr       => $ipv6addr,
+      ipv6_defaultgw => $ipv6gw,
+  }
+}
