12ae7f0a7d9bf49d9507ef3eb4c4f931ba7a615e
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-07-09 15:26:05 +0200

    added some tests

---
+++ spec/functions/g_server_get_side_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe 'g_server::get_side', :type => :puppet_function do
+    let(:pre_condition) { 'class { "g_server": external_ifaces => ["OUT"], internal_ifaces => ["IN"]}' }
+    context "should split available interfaces" do
+        it { is_expected.to run.with_params('dummyXX').and_return('none') }
+        it { is_expected.to run.with_params('IN').and_return('internal') }
+        it { is_expected.to run.with_params('OUT').and_return('external') }
+    end
+end
