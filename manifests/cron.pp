c2e34e2af2961300c6e6a6420ce489a1b45e5252
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-07-13 19:49:36 +0200

    set default cron service_name on gentoo

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,8 +1,25 @@
 class g_server::cron (
-  Hash[String, Hash] $jobs = {}
+  Hash[String, Hash] $jobs = {},
+  Optional[String] $service_name = undef
 ){
+  case $::osfamily {
+    'Gentoo': {
+      $_defaults = {
+        'service_name' => 'cronie'
+      }
+    }
+    default: {
+      $_defaults = {}
+    }
+  }
+
+  $_user_opts = {
+    'service_name' => $service_name
+  }
+
   class { '::cron':
     manage_package => true,
+    *              => delete_undef_values(merge($_defaults, $_user_opts)),
   }
   create_resources(g_server::cron::job, $jobs)
 }
