32473f464fe718cafccb0c41e1cfa295ed31148b
Arkadiusz Dzięgiel <arkadiusz.dziegiel@glorpen.pl>
2019-07-13 19:39:34 +0200

    support managing hostname on gentoo dist

--- manifests/network/hostname.pp
+++ manifests/network/hostname.pp
@@ -1,13 +1,28 @@
 class g_server::network::hostname(
   $hostname
 ){
-  file { '/etc/hostname':
-    content => "${hostname}\n",
-  }
-  ~> exec { 'g_server update hostname':
-    command     => 'hostname -F /etc/hostname',
-    path        => ['/bin', '/usr/bin', '/usr/local/bin'],
-    refreshonly => true,
-    require     => Package['hostname']
+  case $::osfamily {
+    'Gentoo': {
+      ensure_resource('service', 'hostname', {
+        'ensure'  => 'running',
+        'enabled' => true
+      })
+
+      file { '/etc/conf.d/hostname':
+        content => "# Set to the hostname of this machine\nhostname=\"${hostname}\"\n",
+      }
+      ~>Service['hostname']
+    }
+    default: {
+      file { '/etc/hostname':
+        content => "${hostname}\n",
+      }
+      ~> exec { 'g_server update hostname':
+        command     => 'hostname -F /etc/hostname',
+        path        => ['/bin', '/usr/bin', '/usr/local/bin'],
+        refreshonly => true,
+        require     => Package['hostname']
+      }
+    }
   }
 }
